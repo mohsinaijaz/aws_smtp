ca731c723c760c5d5d09fbe9cee6f65bb4cd91db
Mohsin Aijaz <mohsinaijaz@gmail.com>
2020-01-15 21:03:45 -0500

    adding ssh user1

--- jenkins-job-dsl/smtp.groovy
+++ jenkins-job-dsl/smtp.groovy
@@ -1,19 +1,5 @@
     pipelineJob("smtp-pipeline") {
         parameters {
-          string {
-              name 'OLD_SMPT_1'
-              defaultValue null
-              description 'First SMPT ID to terminate'
-              trim true
-          }
-          string {
-              name 'OLD_SMPT_2'
-              defaultValue null
-              description 'Second SMPT ID to terminate'
-              trim true
-            }
-        }
-        parameters {
           credentials credentialType: 'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey', defaultValue: 'goldkey', description: '', name: 'superuser', required: true
         }
         definition {
