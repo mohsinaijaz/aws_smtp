82b0e96a1b726c7a328b6664c3349ab81ab546b9
Mohsin Aijaz <mohsinaijaz@gmail.com>
2020-01-22 18:09:54 -0500

    adding more

--- jenkins-job-dsl/smtp.groovy
+++ jenkins-job-dsl/smtp.groovy
@@ -1,13 +1,13 @@
     pipelineJob("smtp-pipeline") {
       parameters {
         string {
-            name 'OLD_SMPT_1'
+            name 'OLD_SMTP_1'
             defaultValue null
             description 'First SMPT ID to terminate'
             trim true
         }
         string {
-            name 'OLD_SMPT_2'
+            name 'OLD_SMTP_2'
             defaultValue null
             description 'Second SMPT ID to terminate'
             trim true
