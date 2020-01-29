6392eea4559be4a2a1884e9d40af2abf522fd7d5
Mohsin Aijaz <mohsinaijaz@gmail.com>
2020-01-28 22:38:55 -0500

    fixing syntax

--- jenkins-job-dsl/buildnew_smtp.groovy
+++ jenkins-job-dsl/buildnew_smtp.groovy
@@ -1,4 +1,12 @@
 pipelineJob('stages/buildnew_smtp') {
+    parameters {
+          string {
+            name 'SMTP_ENI'
+            defaultValue ""
+            description 'SMPT ENI Attachment ID'
+            trim true
+          }
+        }
         definition {
             cpsScm {
                 lightweight(false)
