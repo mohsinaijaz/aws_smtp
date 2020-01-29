0e7bb57956373aea8f31d6f311fd9255f3f78694
Mohsin Aijaz <mohsinaijaz@gmail.com>
2020-01-29 13:19:24 -0500

    fixing syntax

--- test-instance/launch_smtp1.sh
+++ test-instance/launch_smtp1.sh
@@ -36,7 +36,7 @@ fi
 
 /usr/local/bin/aws ec2 wait instance-status-ok --instance-ids $instance_id
 
-/usr/local/bin/aws ec2 attach-network-interface --network-interface-id $SMTP_ENI_NET_ID --instance-id $instance_id --device-index 1
+/usr/local/bin/aws ec2 attach-network-interface --network-interface-id $SMTP_ENI_NET_ID --instance-id $instance_id --device-index 1 --quiet
 
 instance_ip=$(/usr/local/bin/aws ec2 describe-instances --instance-ids $instance_id \
 --query 'Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddresses[0].PrivateIpAddress')
